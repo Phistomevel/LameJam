e5818572d6d42cbedf27bb3ee402b7e7d6dc5dc0
ich <ich@localhost.com>
2024-01-07 03:19:32 +0100

    3:20 gang

--- LawnmoverCollider.gd
+++ LawnmoverCollider.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var lifesteal = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -11,10 +12,16 @@ func _process(delta):
 
 
 func _on_area_entered(area):
-	if not get_parent().isDashing:
-		print("you died")
-		return 0
 	var xpbar = get_parent().get_child(0).get_child(0)
 	print(area.get_parent().Speed)
 	xpbar.setEXP(xpbar.getEXP() + ceil(area.get_parent().Speed))
+	lifesteal += 0.1
+	if lifesteal>=1:
+		lifesteal -= 1
+		xpbar.setHP(xpbar.getHP()+1)
+	pass # Replace with function body.
+
+
+func _on_harmed(area):
+	get_parent().get_child(0).get_child(0).setHP(get_parent().get_child(0).get_child(0).getHP()-1)
 	pass # Replace with function body.
